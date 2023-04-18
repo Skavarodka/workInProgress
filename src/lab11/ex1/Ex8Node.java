6364bc38e63e63589fd97a25c84fe7392a79405f
Konstantin.Sokolov.Work <saloshlebom@yandex.ru>
2023-04-18 15:27:38 +0500

    lr11 in progress

--- src/lab11/ex1/Ex8Node.java
+++ src/lab11/ex1/Ex8Node.java
@@ -18,10 +18,118 @@ package lab11.ex1;
 //        •	вывод (возвращается строка, сформированная из элементов списка) toStringRec().
 
 
-import lab11.test.Node;
+
 
 public class Ex8Node {
+    static  Node head = null;
+    public static String toStrings() {
+        String str = "";
+        Node ref = head;
+        while(ref != null){
+            str = str + " " + ref.value;
+            ref = ref.next;
+        }
+        return str;
+    }
+
+    public static void toStringRec(Node ref) {
+        String str = "";
+        if (ref == null) {
+            System.out.println();
+        } else {
+            str += ref.value;
+            ref = ref.next;
+            toStringRec(ref);
+        }
+        System.out.print(str);
+    }
+
+    public static void createHead() {
+        for (int i = 9; i >= 0; i--) {
+            head = new Node(i, head);
+        }
+    }
+
+    public static void createHeadRec(int count) {
+        if (count != 0) {
+            head = new Node(count, head);
+            count--;
+            createHeadRec(count);
+        }
+    }
+
+    public static void createTail() {
+        for (int i = 0; i < 10; i++) {
+            head = new Node(i, head);
+        }
+    }
+
+    public static Node createTailRec(Node ref, int count) {
+        if (count != 0) {
+            ref.next = createTailRec(new Node(count - 1, null), count -1 );
+        }
+        return ref;
+    }
+
+    public static void addFirst(int val) {
+        head = new Node(val, head);
+    }
+
+    public static void addLast(int val) {
+        Node ref = head;
+        while (ref.next != null) {
+           ref = ref.next;
+        }
+        ref.next = new Node(val, null);
+    }
+
+    public static void insert(int val, int value) {
+        Node ref = head;
+        int count = 1;
+        Node nodNext = new Node(value, null);
+        while (ref.next != null && count < val) {
+            ref = ref.next;
+            count++;
+        }
+        nodNext.next = ref.next;
+        ref.next = nodNext;
+    }
+
+    public static void removeFirst() {
+        head = head.next;
+    }
+
+    public static void removeLast() {
+        Node ref = head;
+        while (ref.next.next != null) {
+            ref = ref.next;
+        }
+        ref.next = null;
+    }
+
+    public static void remove(int x) {
+        Node ref = head;
+        int count = 1;
+        while (ref.next != null && count < x) {
+            ref = ref.next;
+            count++;
+        }
+        ref.next = ref.next.next;
+    }
     public static void main(String[] args) {
-        Runnable hello_world = () -> System.out.println("hello world");
+        //createHeadRec(5);
+        createHead();
+        //createTail();
+        addFirst(404);
+        addLast(101);
+        insert(5, 202);
+        removeFirst();
+        removeLast();
+        remove(4);
+        Node ref = head;
+        System.out.println(toStrings());
+        toStringRec(ref);
+        System.out.println();
+        toStringRec(createTailRec(new Node(5, null),5));
     }
 }
