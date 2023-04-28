8c403d49c95a8c64f8881d8fa86ee0903f48271f
Konstantin.Sokolov.Work <saloshlebom@yandex.ru>
2023-04-28 16:50:11 +0500

    lr10 in progress

--- src/lab10/Ex1_/Main_Ex1.java
+++ src/lab10/Ex1_/Main_Ex1.java
@@ -9,31 +9,93 @@ import org.xml.sax.SAXException;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.OutputKeys;
 import javax.xml.transform.Transformer;
 import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
+import javax.xml.xpath.*;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Main_Ex1 {
     public static void main(String[] args) throws ParserConfigurationException, SAXException, IOException {
+
         DocumentBuilderFactory factory = DocumentBuilderFactory.newDefaultInstance();
         DocumentBuilder builder = factory.newDocumentBuilder();
-        Document doc = builder.parse(new File("src/lab10/Ex1_/example.xml"));
+        String filePath = "src/lab10/Ex1_/example.xml";
+        Document doc = builder.parse(filePath);
+        doc.getDocumentElement().normalize();
+
         try {
+            //addNewBooks(doc);
+            searchElementByYear(doc);
+            doc.getDocumentElement().normalize();
             Transformer transformer = TransformerFactory.newInstance().newTransformer();
             DOMSource source = new DOMSource(doc);
-            StreamResult result = new StreamResult(new File("src/lab10/Ex1_/example.xml"));
+            StreamResult result = new StreamResult(new File("src/lab10/Ex1_/example2.xml"));
+            transformer.setOutputProperty(OutputKeys.INDENT, "yes");
             transformer.transform(source, result);
+            System.out.println("Write complete");
         } catch (Exception e) {
             e.printStackTrace();
         }
-        NodeList nodeList = doc.getElementsByTagName("TEST");
-
 
     }
+
+    public static void searchElementByYear(Document document) throws XPathExpressionException {
+        List<String> list = new ArrayList<>();
+        XPathFactory xPathFactory = XPathFactory.newInstance();
+        XPath xPath = xPathFactory.newXPath();
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter the year for search");
+        XPathExpression xPathExpression = xPath.compile("/library/book[year=" + in.nextInt() + "]/title/text()");
+        NodeList nodeList = (NodeList) xPathExpression.evaluate(document, XPathConstants.NODESET);
+        for (int i = 0; i < nodeList.getLength(); i++) {
+            list.add(nodeList.item(i).getNodeValue());
+        }
+        System.out.println(list);
+    }
+    public static void addNewBooks(Document doc) {
+        NodeList rootElement = doc.getElementsByTagName("library");
+        Element elementRoot = null;
+        Scanner in = new Scanner(System.in);
+        for (int i = 0; i < rootElement.getLength(); i++) {
+            elementRoot = (Element) rootElement.item(i);
+
+        System.out.println("Enter book name");
+        Element book = doc.createElement("book");
+        elementRoot.appendChild(book);
+
+        Element title = doc.createElement("title");
+        title.appendChild(doc.createTextNode(in.nextLine()));
+            book.appendChild(title);
+
+        System.out.println("Enter author name");
+        Element author = doc.createElement("author");
+        author.appendChild(doc.createTextNode(in.nextLine()));
+            book.appendChild(author);
+
+        System.out.println("Enter year");
+        Element year = doc.createElement("year");
+        year.appendChild(doc.createTextNode(in.nextLine()));
+            book.appendChild(year);
+
+        System.out.println("Books add");}
+    }
+
+//            try {
+//            Transformer transformer = TransformerFactory.newInstance().newTransformer();
+//            DOMSource source = new DOMSource(doc);
+//            StreamResult result = new StreamResult(new File(filePath));
+//            transformer.transform(source, result);
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//        NodeList nodeList = doc.getElementsByTagName("TEST");
 
 //    List<Element> books = nodeList.stream()
 //            .filter(node -> node.getNodeType() == Node.ELEMENT_NODE)
